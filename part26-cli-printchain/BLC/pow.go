08242f3bf4344f187d8ecde5b7c337056e8f46b6
alvin-kun <1196859010@qq.com>
2019-01-06 15:42:27 +0800

    Add cli usage and method to judge the existing of database.

---
+++ part26-cli-printchain/BLC/pow.go
@@ -0,0 +1,81 @@
+package BLC
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+var (
+	//定义Nonce最大值
+	maxNonce = math.MaxInt64
+)
+
+const targetBits = 20
+
+type ProofOfWork struct {
+	block  *Block   //当前需要验证的区块
+	target *big.Int //大数存储
+}
+
+//数据拼接
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.HashTransactions(),
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+//ProofOfWork对象的方法
+func (pow *ProofOfWork) Run() (int, []byte) {
+
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+	fmt.Printf("Start mining new block \n")
+
+	for nonce < maxNonce {
+
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+
+	return nonce, hash[:]
+}
+
+//工厂方法
+func NewProofOfWork(block *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	pow := &ProofOfWork{block, target}
+	return pow
+}
+
+//验证当前的工作量证明的有效性
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+	return isValid
+}
